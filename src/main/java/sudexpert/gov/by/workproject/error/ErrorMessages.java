085670835a2cb82c323d94059cddc5d0910fe5c1
myaszh <myasz@gmail.com>
2025-09-09 12:35:42 +0300

    эксепшн хэндлер чэнжд

--- src/main/java/sudexpert/gov/by/workproject/error/ErrorMessages.java
+++ src/main/java/sudexpert/gov/by/workproject/error/ErrorMessages.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorMessages {
     public static final String RESOURCE_NOT_FOUND_BY_ID_MESSAGE = "%s with id %d does not exist";
-    public static final String RESOURCE_NOT_FOUND_BY_CUSTOMER_NAME_MESSAGE = "%s with customer name %s does not exist";
+    public static final String RESOURCE_NOT_FOUND_BY_CUSTOMER_NAME_MESSAGE = "%s with ID %s does not exist";
     public static final String DUPLICATE_RESOURCE_MESSAGE = "%s with this %s already exists";
 }
--- src/main/java/sudexpert/gov/by/workproject/exception/GlobalExceptionHandler.java
+++ src/main/java/sudexpert/gov/by/workproject/exception/GlobalExceptionHandler.java
@@ -2,18 +2,44 @@ package sudexpert.gov.by.workproject.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sudexpert.gov.by.workproject.dto.error.AppError;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({ResourceNotFoundException.class})
-    public ResponseEntity<String> handlerResourceNotFound(ResourceNotFoundException notFoundException){
-        return new ResponseEntity<>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<AppError> handlerResourceNotFound(ResourceNotFoundException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({DuplicateResourceException.class})
-    public ResponseEntity<String> handlerDuplicateResource(DuplicateResourceException duplicateResourceException){
-        return new ResponseEntity<>(duplicateResourceException.getMessage(), HttpStatus.CONFLICT);
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<AppError> handlerDuplicateResource(DuplicateResourceException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<AppError> handleValidationException(MethodArgumentNotValidException ex) {
+        return buildErrorResponse("Некорректные данные в запросе.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class) // общий обработчик
+    public ResponseEntity<AppError> handleGeneralException(Exception ex) {
+        return buildErrorResponse("Произошла внутренняя ошибка. Пожалуйста, попробуйте позже.",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<AppError> buildErrorResponse(String message, HttpStatus status) {
+        AppError error = AppError.builder()
+                .status(status.value())
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity.status(status).body(error);
     }
 }
