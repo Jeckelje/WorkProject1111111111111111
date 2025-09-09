ec44ba00d29b0e6e4c087469d94204f51c579521
myaszh <myasz@gmail.com>
2025-09-09 14:38:53 +0300

    поезд сервайс импл

--- src/main/java/sudexpert/gov/by/workproject/repository/TrainRepository.java
+++ src/main/java/sudexpert/gov/by/workproject/repository/TrainRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sudexpert.gov.by.workproject.model.Train;
 
+import java.util.List;
+
 @Repository
 public interface TrainRepository extends JpaRepository<Train, Long> {
+
+    List<Train> findTrainsByWorkerId(Long workerId);
+
 }
